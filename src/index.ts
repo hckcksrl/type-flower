25ebd729a259f66d59ff02282d53c10a6b64673b
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-22 22:06:45 +0900

    Index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import "reflect-metadata";
+import { Apollo, App } from "./app";
 import { createConnection } from "typeorm";
 
+const server = new Apollo().server;
+const app = new App().express;
+
+server.applyMiddleware({ app });
+
 createConnection()
-  .then(async connection => {})
-  .catch(error => console.log(error));
+  .then(() => {
+    app.listen({ port: 4000 }, () =>
+      console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
+    );
+  })
+  .catch(err => {
+    console.log(err);
+  });
