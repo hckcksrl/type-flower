2fbed294bc67c1bda3aa3a503d37d728cdbb9896
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-22 22:06:42 +0900

    App

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,50 @@
 import { ApolloServer } from "apollo-server-express";
-import { GraphQLServer } from "graphql-yoga";
 import schema from "./schema";
 import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+import DecodeJwt from "./helper/decodejwt";
 
-class App {
-  public server: GraphQLServer;
+export class Apollo {
+  public server: ApolloServer;
   constructor() {
-    this.server = new GraphQLServer({
+    this.server = new ApolloServer({
       schema,
-      context: (req): any => {
-        const { connection: { context = null } = {} } = req;
+      context: ({ req }): any => {
         return {
-          req: req.request
+          req: req.body.user
         };
       }
     });
   }
 }
 
-export default new App().server;
+export class App {
+  public express: express.Application;
+  constructor() {
+    this.express = express();
+    this.middleware();
+  }
+  private middleware = (): void => {
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: true }));
+    this.express.use(cors());
+    this.express.use(this.jwt);
+  };
+  private jwt = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ): Promise<void> => {
+    const token = req.get("authorization");
+    if (token) {
+      const user = await DecodeJwt(token);
+      if (user) {
+        req.body.user = user;
+      } else {
+        req.body.user = undefined;
+      }
+    }
+    next();
+  };
+}
