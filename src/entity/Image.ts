89b84b7732f7142709eebfb2d7d81272794e0ea9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-03-22 22:07:14 +0900

    Image Entity

--- src/entity/Image.ts
+++ src/entity/Image.ts
@@ -3,9 +3,13 @@ import {
   PrimaryGeneratedColumn,
   Column,
   BaseEntity,
-  ManyToOne
+  ManyToOne,
+  ManyToMany,
+  JoinTable
 } from "typeorm";
 import { Users } from "./Users";
+import { Flowers } from "./Flowers";
+import { Flower_Image } from "./Flower_Image";
 
 @Entity({ name: "images" })
 export class Images extends BaseEntity {
@@ -15,6 +19,17 @@ export class Images extends BaseEntity {
   @Column({ type: "varchar", nullable: false })
   image: string;
 
-  @ManyToOne(type => Users, users => users.id)
-  users: Users[];
+  @ManyToOne(type => Users, users => users.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
+  users: Users;
+
+  @ManyToOne(type => Flower_Image, flower_image => flower_image.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
+  flower_image: Flower_Image[];
 }
