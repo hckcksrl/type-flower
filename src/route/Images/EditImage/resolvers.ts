832558778281691035046f7b9171779f14fdb611
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-04-03 18:43:09 +0900

    EditImage Resolvers

---
+++ src/route/Images/EditImage/resolvers.ts
@@ -0,0 +1,47 @@
+import { Resolvers } from "../../../types/resolvers";
+import { EditImageResponse, EditImageArgs } from "../../../types/graphql";
+import { Users } from "../../../entity/Users";
+import { Images } from "../../../entity/Image";
+import { Flowers } from "../../../entity/Flowers";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EditImage: async (
+      _,
+      args: EditImageArgs,
+      { req }
+    ): Promise<EditImageResponse> => {
+      const user: Users = req;
+      try {
+        let image: Images = await Images.findOne({ id: args.id });
+        if (image) {
+          if (args.flowerid) {
+            const flower: Flowers = await Flowers.findOne({
+              id: args.flowerid
+            });
+            image.flowers = flower;
+          }
+          image.image = args.image;
+          image.content = args.content;
+          image.save();
+          return {
+            result: true,
+            error: undefined
+          };
+        } else {
+          return {
+            result: false,
+            error: "Image Not Exist"
+          };
+        }
+      } catch (error) {
+        return {
+          result: false,
+          error: error.message
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
