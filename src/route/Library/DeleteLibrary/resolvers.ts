9315939a523467fdf26a4b45934c81e8bb49c37c
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-04-03 18:43:27 +0900

    DeleteLibrary Resolvers

---
+++ src/route/Library/DeleteLibrary/resolvers.ts
@@ -0,0 +1,51 @@
+import { Resolvers } from "../../../types/resolvers";
+import {
+  DeleteLibraryResponse,
+  DeleteLibraryArgs
+} from "../../../types/graphql";
+import { Users } from "../../../entity/Users";
+import { Librarys } from "../../../entity/Librarys";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    DeleteLibrary: async (
+      _,
+      args: DeleteLibraryArgs,
+      { req }
+    ): Promise<DeleteLibraryResponse> => {
+      const user: Users = req;
+      try {
+        const library: Librarys = await Librarys.findOne(
+          { id: args.id },
+          { relations: ["users"] }
+        );
+        if (library) {
+          if (library.users.id === user.id) {
+            library.remove();
+            return {
+              result: true,
+              error: undefined
+            };
+          } else {
+            return {
+              result: false,
+              error: "not authentication"
+            };
+          }
+        } else {
+          return {
+            result: false,
+            error: "not exist"
+          };
+        }
+      } catch (error) {
+        return {
+          result: false,
+          error: error.message
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
