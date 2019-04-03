f6d00abf0ed23ff6f34dbc506799601fad9d58dd
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-04-03 18:41:57 +0900

    Users Entity

--- src/entity/Users.ts
+++ src/entity/Users.ts
@@ -11,6 +11,7 @@ import { Images } from "./Image";
 import { Librarys } from "./Librarys";
 import { Likes } from "./Likes";
 import { Comment } from "./Comment";
+import { Recent } from "./Recent";
 
 @Entity({ name: "users" })
 export class Users extends BaseEntity {
@@ -23,17 +24,40 @@ export class Users extends BaseEntity {
   @Column({ type: "varchar", nullable: false })
   password: string;
 
-  @OneToMany(type => Images, images => images.id)
+  @OneToMany(type => Images, images => images.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
   images: Images[];
 
-  @OneToMany(type => Librarys, librarys => librarys.id)
+  @OneToMany(type => Librarys, librarys => librarys.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
   librarys: Librarys[];
 
-  @OneToMany(type => Likes, like => like.id)
+  @OneToMany(type => Likes, like => like.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
   like: Likes[];
 
-  @OneToMany(type => Comment, comment => comment.id)
+  @OneToMany(type => Comment, comment => comment.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
   comment: Comment[];
+
+  @OneToMany(type => Recent, recent => recent.id, {
+    cascade: true,
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE"
+  })
+  recent: Recent[];
 
   @CreateDateColumn()
   createUser: string;
